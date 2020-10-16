455836d50867ca1bead8a0c2396d85646724c314
danimonus <55405619+danimonus@users.noreply.github.com>
2020-10-16 15:55:51 +0200

    Add files via upload

--- HF2/F6/Teszt.java
+++ HF2/F6/Teszt.java
@@ -9,9 +9,7 @@ public class Teszt {
         int flag = 0;
 
         for (int i = 0; i < values.length - 4; i++) {
-            System.out.println(values[i] +" "+ values[1] +" "+ values[2]+" "+ values[3] +" "+ values[4]);
-            if (values[i] == values[i + 1] && values[i] == values[i + 2]
-                    && values[i] == values[i + 3]) {
+            if (values[i] == values[i + 1] && values[i] == values[i + 2] && values[i] == values[i + 3]) {
                 flag = 1;
             }
 
